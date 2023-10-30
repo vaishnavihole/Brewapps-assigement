c7655195c6af28e5c7a167ccff6239586b7f8f1f
vaishnavihole <vaishnavihole1@gmail.com>
2023-10-30 13:41:43 +0530

    GET api added to fetched all books

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import Book from "./models/Book.js";
 
 const app = express()
 app.use(express.json());
@@ -15,12 +16,26 @@ async function connectMongoDB() {
 connectMongoDB();
 
 const PORT = process.env.PORT || 5000;
+
+// health api
 app.get("/health", (req, res) => {
     res.json({
         success: true,
         message: "All Good🏆",
     });
 });
+
+// GET /books
+app.get("/books", async (req, res) => {
+    const books = await Book.find()
+
+    res.json({
+        success: true,
+        data: books,
+        message: "Books fetched successfully"
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`The server is Running on Port ${PORT} 🚀`);
